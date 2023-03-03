463b1a6a657e32576f2712b5757a801fdf065ba3
ADAMSINSUAT <109055704+ADAMSINSUAT@users.noreply.github.com>
2023-03-03 14:14:30 +0800

    Created a proper export of the Excel File. Used the sheetjs-style for designing the excel file

--- webpack/src/components/ExportExcel.js
+++ webpack/src/components/ExportExcel.js
@@ -1,5 +1,5 @@
 import * as FileSaver from "file-saver";
-// import XLSX from "sheetjs-style";
+import XLSX from "sheetjs-style";
 import { Tooltip, Button } from "@mui/material";
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
@@ -14,40 +14,26 @@ const ExportExcel = ({ excelData, fileName }) => {
     //Sets the file extension of the excel file
     const fileExtenstion = '.xlsx';
 
-    // //This is called whenever the download button is being pressed
-    // const exportToExcel = async () => {
-
-    //     //It converts the excelData prop into an excel file
-    //     const ws = XLXS.utils.json_to_sheet(excelData);
-
-    //     //Creates the sheets and the sheetnames of the excel file
-    //     const wb = { Sheets: { 'data': ws }, SheetNames: ['data'] };
-
-    //     //Creates the formatting of the excel file and sets its file type as xlsx
-    //     const excelBuffer = XLXS.write(wb, { bookType: "xlsx", type: 'array' });
-
-    //     //It converts the excelBuffer into a blob which will then be prepared for downloading
-    //     const data = new Blob([excelBuffer], { type: fileType });
-
-    //     //Creates an excel file with its name and extension type and displays the data in it
-    //     //It will then be ready for download
-    //     FileSaver.saveAs(data, fileName + fileExtenstion);
-    // }
-
+    //This is called whenever the download button is being pressed
     const exportToExcel = async () => {
-        const worksheet = XLSX.utils.json_to_sheet(excelData);
+        const rows = excelData.map(row => ({
+            UserID: row.UserID,
+            ID: row.ID,
+            Title: row.Title,
+            Body: row.Body
+        }))
+        const worksheet = XLSX.utils.json_to_sheet(rows);
         const workbook = XLSX.utils.book_new();
         XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
 
         /* fix headers */
         XLSX.utils.sheet_add_aoa(worksheet, [["ID", "User ID", "Title", "Body"]], { origin: "A1" });
 
-        /* calculate column width */
-        const max_width = excelData.reduce((w, r) => Math.max(w, r.Body.length), 10);
-        worksheet["!cols"] = [{ wch: max_width }];
+        /* set column width for each object data */
+        worksheet["!cols"] = [{ width: 4 }, { width: 8 }, { width: 70 }, { width: 180 }];
 
-        /* create an XLSX file and try to save to Presidents.xlsx */
-        XLSX.writeFile(workbook, "Presidents.xlsx", { compression: true });
+        /* create an XLSX file and try to save to UserData.xlsx */
+        XLSX.writeFile(workbook, "UserData.xlsx", { compression: true });
     }
 
     return (
